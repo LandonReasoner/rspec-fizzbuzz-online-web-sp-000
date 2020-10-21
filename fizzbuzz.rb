2183fdfc0345966da65955df12c7be66bc14b3bb
Landon Reasoner <reasoner.landon90@gmail.com>
2020-10-21 22:36:26 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,5 +2,13 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  
+  if int % 3 == 0
+    return "Fizz"
+  elsif int % 5 == 0 
+    return "Buzz"
+  elsif int % 15 == 0 
+    return "FizzBuzz"
+  else 
+    nil
+  end
 end
